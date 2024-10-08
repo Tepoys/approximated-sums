b4610585f612ee3073eda63170a9f7a1597ba3b7
Tepoys <137014852+Tepoys@users.noreply.github.com>
2024-10-08 08:57:12 -0400

    added simpsons and midpoint

--- sums.py
+++ sums.py
@@ -1,6 +1,11 @@
 import math
+
+accuracy = 6
+
 def p(x:int):
-    return math.sqrt(x**3 -8)
+    return 9*math.cos((x**2))
+#math.sqrt((x**3)-8)
+#5*math.cos((x**2))
 
 def enterEquation():
     return
@@ -15,27 +20,65 @@ def getParam() -> tuple[int, int, int]:
     return (a,b,n)
 
 def midpoint(tup: tuple[int, int, int]):
-
+    dx = (tup[1]-tup[0])/tup[2]
+    
+    sum = 0
+    for i in range(tup[2]):
+        temp = p(tup[0]+(dx/2)+i*dx)
+        sum += temp
+        
+    sum*=dx
+    
+    print("The midpoint sum is:\n" + str(rounding(sum)))
+    
+    
+    
     return
 
 def trapazoid(tup: tuple[int, int, int]):
     dx = (tup[1]-tup[0])/tup[2]
 
     sum = 0
-    for i in range(tup[2]):
+    for i in range(tup[2]+1):
         temp = p(tup[0]+ (i*dx))
-        if i == 0 or i == tup[2]-1:
+        if i != 0 and i != tup[2]:
             temp*=2
         sum += temp
 
     sum/=2
-    print("The trapazoid sum is:" + sum)
+    sum*=dx
+    print("The trapazoid sum is:\n" + str(rounding(sum)))
     return
 
 def simpsons(tup: tuple[int, int, int]):
-
+    if(tup[2]%2 == 1):
+        print("Can not use simpsons with odd subdivisions!")
+        return
+    
+    dx = (tup[1]-tup[0])/tup[2]
+    
+    sum = 0
+    for i in range(tup[2]+1):
+        temp = p(tup[0]+ (i*dx))
+        if (i != 0 and i != tup[2]):
+            if i%2 == 1:
+                temp*=4
+            else:
+                temp*=2
+        sum += temp
+    
+    sum*=dx
+    sum/=3
+    
+    print("The simpson's sum is:\n" + str(rounding(sum)))
     return
 
+
+def rounding(num:float):
+    return round(num, accuracy)
+
+def changeRounding():
+    rounding = n if (n := input("What would you like rounding to be(current rounding is " + accuracy + ", enter -1 to cancle):")) <= -1 else rounding
 
 
 
@@ -43,18 +86,19 @@ def findSumMenu():
     tup = getParam()
     run = True
     while(run):
-        num = input("What sum would you like to find:\n1.Midpoint\n2.Trapazoid\n3.Simpson's\n4.Left\n5.Right\n6.Exit\n")
+        num = input("What sum would you like to find:\n1.Midpoint\n2.Trapazoid\n3.Simpson's\n4.Left\n5.Right\n6.Back\n")
         num = int(num)
         match num:
+            case 0:
+                midpoint(tup)
+                trapazoid(tup)
+                simpsons(tup)
             case 1:
                 midpoint(tup)
-                break
             case 2:
                 trapazoid(tup)
-                break
             case 3:
                 simpsons(tup)
-                break
 
 
 
@@ -69,19 +113,18 @@ def findSumMenu():
 def menu():
     run = True
     while(run):
-        num = input("What would you like to do\n1.Enter Equation\n2.Check Equation\n3.Find Sum\n4.Exit\n")
+        num = input("What would you like to do\n1.Enter Equation\n2.Check Equation\n3.Find Sum\n4.Change Rounding\n5.Exit\n")
         num = int(num)
         match num:
             case 1:
                 enterEquation()
-                break
             case 2:
                 printEquation()
-                break
             case 3:
                 findSumMenu()
-                break
             case 4:
+                changeRounding()
+            case 5:
                 return
             case _:
                 print("Case \"" + num + "\" not recognized")
